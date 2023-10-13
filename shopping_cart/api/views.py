ce35771ae63d23b73e2b703def1333c9cc780850
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-10-12 22:11:03 -0500

    Feat: New Class CartUpdate & Endpoints

--- shopping_cart/api/views.py
+++ shopping_cart/api/views.py
@@ -32,4 +32,48 @@ class ShoppingCart(View):
 
         print(data)
         return JsonResponse(data, status=201)
+
+    def get(self, request):
+        items_count = CartItem.objects.count()
+        items = CartItem.objects.all()
+        items_data = []
+
+        for item in items:
+            items_data.append({
+                'product_name': item.product_name,
+                'product_price': item.product_price,
+                'product_quantity': item.product_quantity
+            })
+        data = {
+            'items': items_data,
+            'count': items_count,
+
+        }
+        print(items_data)
+        return JsonResponse(data)
     
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ShoppingCartUpdate(View):
+
+    def patch(self, request, item_id):
+        data = json.loads(request.body.decode("utf-8"))
+        item = CartItem.objects.get(id = item_id)
+        item.product_quantity = data["product_quantity"]
+        item.save()
+
+        data = {
+            'message': f'Item {item_id} has been updated'
+        }
+
+        return JsonResponse(data)
+    
+    def delete(self, request, item_id):
+        item = CartItem.objects.get(id = item_id)
+        item.delete()
+
+        data = {
+            'message': f'Item {item_id} has been deleted'
+        }
+
+        return JsonResponse(data)
